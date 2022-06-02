a2eb50baf068c65f9b95e6f911db62bcc643e57c
F999999999 <9811@88.com>
2022-06-02 22:05:31 +0800

    优化：进一步封装 Axios

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,10 +1,22 @@
 import axios from "axios";
+import { getTokenByLocalStorage } from "@/utils/auth";
 
 const http = axios.create({
   baseURL: "http://geek.itheima.net/v1_0",
   timeout: 5000,
 });
 
+// 请求拦截器
+http.interceptors.request.use((config) => {
+  // 获取 token
+  const token = getTokenByLocalStorage();
+  if (token) {
+    config.headers.Authorization = "Basic " + token;
+  }
+  return config;
+});
+
+// 响应拦截器
 http.interceptors.response.use(
   (res) => {
     return res?.data?.data || res;
