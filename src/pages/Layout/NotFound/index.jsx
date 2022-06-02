9f32bb513109cfd1ffb9639f6f1d981c5dd42d83
F999999999 <9811@88.com>
2022-06-02 22:45:38 +0800

    完成：数据面板组件&404组件

--- src/pages/Layout/NotFound/index.jsx
+++ src/pages/Layout/NotFound/index.jsx
@@ -1,7 +1,21 @@
-import styles from "./index.module.scss";
+import { Button, Result } from "antd";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
-  return <div className={styles.root}>404</div>;
+  const navigation = useNavigate();
+  return (
+    <Result
+      style={{ paddingTop: 100 }}
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={
+        <Button onClick={() => navigation("/")} type="primary">
+          返回首页
+        </Button>
+      }
+    />
+  );
 };
 
 export default NotFound;
