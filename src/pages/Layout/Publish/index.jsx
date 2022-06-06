1cf10320d6f0190f51438b86bd91f56c082f0102
F999999999 <9811@88.com>
2022-06-06 18:13:21 +0800

    完成：上传文章封面图片

--- src/pages/Layout/Publish/index.jsx
+++ src/pages/Layout/Publish/index.jsx
@@ -1,9 +1,35 @@
 import styles from "./index.module.scss";
-import { Breadcrumb, Button, Card, Form, Input, Space } from "antd";
+import {
+  Breadcrumb,
+  Button,
+  Card,
+  Form,
+  Input,
+  Radio,
+  Space,
+  Upload,
+} from "antd";
 import { Link } from "react-router-dom";
 import { Channel } from "@/components/Channel";
+import { useState } from "react";
+import { PlusOutlined } from "@ant-design/icons";
 
 const Publish = () => {
+  // 文章封面类型
+  const [type, setType] = useState(1);
+  // 修改文章封面类型
+  const onTypeChange = (e) => {
+    setType(e.target.value);
+    // 清空封面列表
+    setFileList([]);
+  };
+  // 文章封面列表
+  const [fileList, setFileList] = useState([]);
+  // 上传文章封面
+  const onUploadChange = ({ fileList }) => {
+    setFileList(fileList);
+  };
+
   return (
     <div className={styles.root}>
       <Card
@@ -25,6 +51,34 @@ const Publish = () => {
           </Form.Item>
           <Form.Item label="所属频道：" name="channel_id">
             <Channel width={400} />
+          </Form.Item>
+          <Form.Item label="文章封面：">
+            <Form.Item style={{ marginBottom: 0 }}>
+              <Radio.Group value={type} onChange={onTypeChange}>
+                <Radio value={1}>单图</Radio>
+                <Radio value={3}>三图</Radio>
+                <Radio value={0}>无图</Radio>
+              </Radio.Group>
+            </Form.Item>
+            {type > 0 ? (
+              <div style={{ marginTop: 16 }}>
+                <Upload
+                  name="image"
+                  listType="picture-card"
+                  action="http://geek.itheima.net/v1_0/upload"
+                  fileList={fileList}
+                  onPreview={() => {}}
+                  onChange={onUploadChange}
+                >
+                  {fileList.length < type ? (
+                    <div>
+                      <PlusOutlined />
+                      <div style={{ marginTop: 8 }}>上传图片</div>
+                    </div>
+                  ) : null}
+                </Upload>
+              </div>
+            ) : null}
           </Form.Item>
           <Form.Item wrapperCol={{ offset: 4 }}>
             <Space>
