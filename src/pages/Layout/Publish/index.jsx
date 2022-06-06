b8d8dd91df9c0d4d16c8645a088ba155a04c68ed
F999999999 <9811@88.com>
2022-06-06 17:37:23 +0800

    完成：发布文章框架渲染

--- src/pages/Layout/Publish/index.jsx
+++ src/pages/Layout/Publish/index.jsx
@@ -1,7 +1,57 @@
 import styles from "./index.module.scss";
+import { Breadcrumb, Button, Card, Form, Input, Select, Space } from "antd";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getChannels } from "@/store/articleSlice";
+import { useEffect } from "react";
 
 const Publish = () => {
-  return <div className={styles.root}>Publish</div>;
+  const dispatch = useDispatch();
+  // 频道列表数据
+  const channelsList = useSelector((state) => state.article.channels);
+  useEffect(() => {
+    dispatch(getChannels());
+  }, [dispatch]);
+
+  return (
+    <div className={styles.root}>
+      <Card
+        title={
+          <Breadcrumb>
+            <Breadcrumb.Item>
+              <Link to="/">首页</Link>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>
+              <Link to="/article">内容管理</Link>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>发布文章</Breadcrumb.Item>
+          </Breadcrumb>
+        }
+      >
+        <Form labelCol={{ span: 4 }}>
+          <Form.Item label="文章标题：" name="title">
+            <Input placeholder="请输入文章标题" style={{ width: 400 }} />
+          </Form.Item>
+          <Form.Item label="所属频道：" name="channel_id">
+            <Select style={{ width: 400 }}>
+              {channelsList.map((item) => {
+                return (
+                  <Select.Option key={item.id} value={item.id}>
+                    {item.name}
+                  </Select.Option>
+                );
+              })}
+            </Select>
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 4 }}>
+            <Space>
+              <Button type="primary">发表文章</Button>
+            </Space>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
 };
 
 export default Publish;
